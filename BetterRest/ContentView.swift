af98d70933b2743b854c601403adabeda1025f78
Tina <nancy.catty@gmail.com>
2020-04-26 21:38:12 +0200

    Challenge 1 + 2

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State private var showAlert = false
     @State private var alertMsg = ""
     @State private var alertTitle = ""
+    let cupsOfCoffee = (0...10).map(String.init)
+    let cupsOfCoffeeS = ["0","1","2"]
     // static: so it belongs to ContentView
     static var defaultWakeUpTime: Date {
         var component = DateComponents()
@@ -44,42 +46,49 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-        Form {
-            VStack(alignment: .leading, spacing:0){
-                // Wanted wake up time
-                Text("When do you want to wake up?").font(.headline)
-                DatePicker("I want to wake up at",
-                           selection: $wakeupTime,
-                           displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                    .datePickerStyle(WheelDatePickerStyle())
-            }
-            VStack(alignment: .leading, spacing:0){
-                // Wanted length of sleep
-                Text("Amount of desired sleep").font(.headline)
-                Stepper("\(sleepHours, specifier: "%g") hours", value: $sleepHours, in:0...24, step: 1)
-            }
-            VStack(alignment: .leading, spacing:0){
-                // Nr. Coffee drink per day
-                Text("Amount of coffee intake").font(.headline)
-                Stepper(value: $cups, in: 0...20){
-                    if cups < 2 {
-                        Text("\(cups, specifier: "%d") cup")
-                    } else {
-                        Text("\(cups, specifier: "%d") cups")
+            Form {
+                // VStack(alignment: .leading, spacing:0){
+                Section{
+                    // Wanted wake up time
+                    Text("When do you want to wake up?").font(.headline)
+                    DatePicker("I want to wake up at",
+                               selection: $wakeupTime,
+                               displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+                Section{
+                    VStack(alignment: .leading, spacing:0){
+                        // Wanted length of sleep
+                        Text("Amount of desired sleep").font(.headline)
+                        Stepper("\(sleepHours, specifier: "%g") hours", value: $sleepHours, in:0...24, step: 1)
                     }
                 }
+                Section{
+                    // Nr. Coffee drink per day
+                    VStack(alignment: .leading, spacing:0){
+                        Text("Amount of coffee intake").font(.headline)
+                        Picker("Choose cups", selection: $cups) {
+                            ForEach(0..<self.cupsOfCoffee.count) {
+                                Text("\(self.cupsOfCoffee[$0])")
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+//                Section{
+//                    Text("Recommend BedTime = \(alertMsg)").font(.largeTitle)
+//                }
+                
+                                .navigationBarItems(trailing:   Button(action: calculateBedTime) {
+                                    Text("Calculate").foregroundColor(.purple).font(.headline)
+                                })
+                                    .alert(isPresented: $showAlert, content: {
+                                        Alert(title: Text(alertTitle), message: Text(alertMsg), dismissButton: .default(Text("OK")))
+                                    })
             }
+            .navigationBarTitle("Better Rest").preferredColorScheme(.some(.light)).foregroundColor(.blue)
+            
         }
-        .navigationBarTitle("Better Rest").preferredColorScheme(.some(.light)).foregroundColor(.blue)
-        .navigationBarItems(trailing:   Button(action: calculateBedTime) {
-            Text("Calculate").foregroundColor(.purple).font(.headline)
-        })
-            .alert(isPresented: $showAlert, content: {
-                Alert(title: Text(alertTitle), message: Text(alertMsg), dismissButton: .default(Text("OK")))
-            })
-        }
-        
     }
 }
 
