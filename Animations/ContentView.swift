f8e188f46e9ea1628f3b8e2712f3812262a57889
Tina <nancy.catty@gmail.com>
2020-04-30 23:14:22 +0200

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by T  on 2020-04-30.
+//  Copyright © 2020 AlaacLux. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
