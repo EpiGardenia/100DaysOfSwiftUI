8b9e4a4dcd042d8a49eb42e7bd498f175b067872
Tina <nancy.catty@gmail.com>
2020-07-26 21:30:06 +0200

    Initial Commit

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by T  on 2020-07-26.
+//  Copyright © 2020 AlaacLux. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
