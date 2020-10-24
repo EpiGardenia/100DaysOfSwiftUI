82d65b583ff6c579388bfb2f56acc2922899440c
Tina <nancy.catty@gmail.com>
2020-10-24 19:51:00 +0200

    Initial Commit

---
+++ instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instafilter
+//
+//  Created by T  on 2020-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instafilter/instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instafilterApp.swift
+//  instafilter
+//
+//  Created by T  on 2020-10-24.
+//
+
+import SwiftUI
+
+@main
+struct instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
