1dbde807685e13fe058bbc28b332183ef5ef2c8e
Tina <nancy.catty@gmail.com>
2020-11-04 22:52:25 +0100

    Initial Commit

---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by T  on 2020-11-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
