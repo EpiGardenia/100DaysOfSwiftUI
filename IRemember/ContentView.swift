3f41edb00ba3eed1febdba9f19520f4f6082e965
Tina <nancy.catty@gmail.com>
2020-11-09 18:54:28 +0100

    Initial Commit

---
+++ IRemember/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IRemember
+//
+//  Created by T  on 2020-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
