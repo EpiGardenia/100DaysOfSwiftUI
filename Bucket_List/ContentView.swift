5191c5bd8cc16618a6273084137c64385ced265d
Tina <nancy.catty@gmail.com>
2021-01-01 16:19:56 +0100

    Initial Commit

---
+++ Bucket_List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bucket_List
+//
+//  Created by T  on 2021-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
