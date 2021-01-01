5040287f2f065dc0a0966bc8651f61f23da031fb
Tina <nancy.catty@gmail.com>
2021-01-01 15:40:12 +0100

    Initial Commit

---
+++ Hot_Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot_Prospects
+//
+//  Created by T  on 2021-01-01.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
