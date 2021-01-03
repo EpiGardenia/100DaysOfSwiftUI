7c391ed5d670651077575dd5ac96996fd1125586
Tina <nancy.catty@gmail.com>
2021-01-03 08:30:22 +0100

    Result Type

--- Bucket_List/View/ContentView.swift
+++ Bucket_List/View/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         //ComparableView()
         //AccessDirectoryView()
-        EnumView()
+        //EnumView()
+        ResultType()
     }
 }
 
