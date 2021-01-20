47ef7892911b4ee6dd0aeba318edfa37f2fa6b89
Tina <nancy.catty@gmail.com>
2021-01-20 11:39:49 +0100

    Day96

---
+++ Day96_Day99_Proj19/SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by T  on 2021-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       // Day96_alert_sheet_optionals()
+        Day96_groupsLayoutView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day96_Day99_Proj19/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by T  on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
